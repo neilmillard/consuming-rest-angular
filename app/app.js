edac9de7567e8b148a5e56fdf718417ad1a33516
neilmillard <neil@neilmillard.com>
2017-10-13 16:55:00 +0100

    add user management code. extra bower dependencies.

--- app/app.js
+++ app/app.js
@@ -1,9 +1,37 @@
 'use strict';
 
+var app = angular.module('demo',['ui.router','ngStorage']);
+
+app.constant('urls', {
+    BASE: 'http://localhost:8080/',
+    USER_SERVICE_API : 'http://localhost:8080/api/user/'
+});
+
+app.config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'partials/list',
+                controller:'UserController',
+                controllerAs:'ctrl',
+                resolve: {
+                    users: function ($q, UserService) {
+                        console.log('Load all users');
+                        var deferred = $q.defer();
+                        UserService.loadAllUsers().then(deferred.resolve, deferred.resolve);
+                        return deferred.promise;
+                    }
+                }
+            });
+        $urlRouterProvider.otherwise('/');
+    }]
+);
 // Declare app level module which depends on views, and components
-angular.module('demo', [])
-    .controller('Hello', function ($scope, $http) {
-        $http.get('http://localhost:8080/greeting').then(function (response) {
-            $scope.greeting = response.data;
+app.controller('Hello', function ($scope, $http) {
+    $http.get('http://localhost:8080/greeting').then(function (response) {
+        $scope.greeting = response.data;
         })
-    })
+    }
+);
